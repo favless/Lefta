350a54dce7ed2cb04de9fcc8d11fb8301fb30e48
favless <122537119+favless@users.noreply.github.com>
2026-09-05 12:27:54 +0300

    Day total expenses in calendar

--- src/components/logic/DayEntry.tsx
+++ src/components/logic/DayEntry.tsx
@@ -1,3 +1,4 @@
+import { useExpenses } from "../../context/ExpenseContext.tsx";
 import { useSession } from "../../context/SessionContext.tsx";
 import style from "../../css/logic/DayEntry.module.css";
 
@@ -11,12 +12,21 @@ function DayEntry(props: dayProps) {
   const { selectedDate, setSelectedDate } = useSession();
   const selected = selectedDate === props.date;
 
+  const { expenses } = useExpenses();
+  const dayTotal = Number(
+    expenses
+      .filter((expense) => expense.date === props.date)
+      .reduce((total, expense) => total + expense.amount, 0)
+      .toFixed(2),
+  );
+
   return (
     <div
       className={`${style.container} ${!props.focused ? style.unfocused : ""} ${selected ? style.selected : ""}`}
       onClick={() => setSelectedDate(props.date)}
     >
       <span className={style.day}>{day}</span>
+      <span className={style.total}>{dayTotal <= 0 ? "" : `$${dayTotal}`}</span>
     </div>
   );
 }
